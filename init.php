2e41e482c53f552dafca7149f3e1470c84e469b6
Evgeniy Panov <evgeniy.panov@inplat.ru>
2019-02-14 15:41:30 +0300

    почситил файлы

--- init.php
+++ init.php
@@ -1,4 +1,22 @@
 <?php
+
+// $dbname='tour';
+// $host='localhost';
+// $user='ghost';
+// $password='qwerty';
+// $port='21112';
+// 
+// $db = new PDO("mysql:host=$host;dbname=$dbname;port=$port",$user,$password);
+// 
+// if (!$db) {
+//   echo "Произошла ошибка";
+//   exit;
+// }
+// else {
+//     echo "Успешно подключено";
+// }
+
+
 $user='ghost';
 $pass = 'qwerty';
 $port="21113";
