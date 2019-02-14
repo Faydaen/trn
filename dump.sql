298edbdeb495a4e9bfd90962d85bf85748bb142d
Ghost <faydaen@inbox.ru>
2019-02-14 03:31:59 +0300

    dump 2

--- dump.sql
+++ dump.sql
@@ -1,82 +1,93 @@
 
+create table pers
+(
+  id        int auto_increment
+    primary key,
+  name      varchar(128)        null,
+  sex       tinyint             null,
+  alignment varchar(64)         null,
+  universe  varchar(64)         null,
+  power     float default '10'  not null,
+  isAlive   tinyint default '1' null,
+  buttle    json                null,
+  image     varchar(64)         null
+);
 
 
 INSERT INTO `tour`.`pers` (`name`, `sex`, `alignment`, `universe`) VALUES 
 -- mortal kombat
 ('Соня Блейд', 'f', 'good', 'mortal kombat'),
 ('Сабизо', 'm', 'good', 'mortal kombat'),
-('Скорпион', 'm', 'good', 'mortal kombat'),
+('Скорпион', 'm', 'evil', 'mortal kombat'),
 ('Рейдан', 'm', 'good', 'mortal kombat'),
-('Шао Кан', 'm', 'good', 'mortal kombat'),
+('Шао Кан', 'm', 'evil', 'mortal kombat'),
 ('Джони Кейдж', 'm', 'good', 'mortal kombat'),
-('Шансунг', 'm', 'good', 'mortal kombat'),
-('Кваньчи', 'm', 'good', 'mortal kombat'),
+('Шансунг', 'm', 'evil', 'mortal kombat'),
+('Кваньчи', 'm', 'evil', 'mortal kombat'),
 ('Найт Вульф', 'm', 'good', 'mortal kombat'),
 ('Лю Кан', 'm', 'good', 'mortal kombat'),
-('Синдел', 'f', 'good', 'mortal kombat'),
-('Шива', 'f', 'good', 'mortal kombat'),
-('Шинок', 'm', 'good', 'mortal kombat'),
-('Горо', 'm', 'good', 'mortal kombat'),
+('Синдел', 'f', 'evil', 'mortal kombat'),
+('Шива', 'f', 'evil', 'mortal kombat'),
+('Шинок', 'm', 'evil', 'mortal kombat'),
+('Горо', 'm', 'evil', 'mortal kombat'),
 
 -- rick and morty
 ('Самер Смит', 'f', 'good', 'rick and morty'),
 ('Морти Смит', 'm', 'good', 'rick and morty'),
-('Рик Санчес', 'm', 'good', 'rick and morty'),
+('Рик Санчес', 'm', 'neutral', 'rick and morty'),
 
 -- harry potter
 ('Гарри Поттер', 'm', 'good', 'harry potter'),
 ('Рон Узли', 'm', 'good', 'harry potter'),
-('Волон Де Морт', 'm', 'good', 'harry potter'),
-('Драко Малфой', 'm', 'good', 'harry potter'),
+('Волон Де Морт', 'm', 'evil', 'harry potter'),
+('Драко Малфой', 'm', 'evil', 'harry potter'),
 ('Гермиона Грейджер', 'f', 'good', 'harry potter'),
 ('Миневра Макгонокал', 'f', 'good', 'harry potter'),
-('Беллатриса Лестрейндж', 'f', 'good', 'harry potter'),
+('Беллатриса Лестрейндж', 'f', 'evil', 'harry potter'),
 
 -- star wars
 ('Йода', 'm', 'good', 'star wars'),
-('Боба Фет', 'm', 'good', 'star wars'),
+('Боба Фет', 'm', 'evil', 'star wars'),
 ('Оби Ван Кеноби', 'm', 'good', 'star wars'),
 ('Квай Гон Джин', 'm', 'good', 'star wars'),
-('Палпатин', 'm', 'good', 'star wars'),
-('Дарт Мол', 'm', 'good', 'star wars'),
-('Дарт Вейдер', 'm', 'good', 'star wars'),
+('Палпатин', 'm', 'evil', 'star wars'),
+('Дарт Мол', 'm', 'evil', 'star wars'),
+('Дарт Вейдер', 'm', 'evil', 'star wars'),
 ('Люк Скайвокер', 'm', 'good', 'star wars'),
-('Кайло Рен', 'm', 'good', 'star wars'),
+('Кайло Рен', 'm', 'evil', 'star wars'),
 ('Рей', 'f', 'good', 'star wars'),
-('Сноук', 'm', 'good', 'star wars'),
+('Сноук', 'm', 'evil', 'star wars'),
 ('Лея Органо', 'm', 'good', 'star wars'),
-('Хан Соло', 'm', 'good', 'star wars'),
-('Хакс', 'm', 'good', 'star wars'),
-('Фазма', 'f', 'good', 'star wars'),
+('Хакс', 'm', 'evil', 'star wars'),
+('Фазма', 'f', 'evil', 'star wars'),
 ('Падме', 'f', 'good', 'star wars'),
-('Джа-Джа Бинкс', 'm', 'good', 'star wars'),
+('Джа-Джа Бинкс', 'm', 'neutral', 'star wars'),
 ('Ахсока Тано', 'f', 'good', 'star wars'),
-('Асаш Вентрас', 'f', 'good', 'star wars'),
-('Шаак Ти', 'f', 'good', 'star wars'),
+('Асаш Вентрас', 'f', 'evil', 'star wars'),
 ('Аяла Секура', 'f', 'good', 'star wars'),
-('Гривус', 'm', 'good', 'star wars'),
+('Гривус', 'm', 'evil', 'star wars'),
 
 -- game of thrones
 ('Джон Сноу', 'm', 'good', 'game of thrones'),
 ('Дейнерис Таргаран', 'f', 'good', 'game of thrones'),
 ('Арья Старк', 'f', 'good', 'game of thrones'),
 ('Санса Старк', 'f', 'good', 'game of thrones'),
-('Визейрес Таргариан', 'm', 'good', 'game of thrones'),
-('Питер Бейлиш', 'm', 'good', 'game of thrones'),
-('Варис', 'm', 'good', 'game of thrones'),
-('Серсея Ланистр', 'f', 'good', 'game of thrones'),
-('Рамси Сноу', 'm', 'good', 'game of thrones'),
-('Дрого', 'm', 'good', 'game of thrones'),
+('Визейрес Таргариан', 'm', 'evil', 'game of thrones'),
+('Питер Бейлиш', 'm', 'evil', 'game of thrones'),
+('Варис', 'm', 'neutral', 'game of thrones'),
+('Серсея Ланистр', 'f', 'evil', 'game of thrones'),
+('Рамси Сноу', 'm', 'evil', 'game of thrones'),
+('Дрого', 'm', 'neutral', 'game of thrones'),
 ('Джофри Баратеон', 'm', 'good', 'game of thrones'),
 ('Тирион Ланистр', 'm', 'good', 'game of thrones'),
-('Милисандра', 'f', 'good', 'game of thrones'),
+('Милисандра', 'f', 'evil', 'game of thrones'),
 
 -- family guy
 ('Лоис Грифин', 'f', 'good', 'family guy'),
 ('Питер Грифен', 'm', 'good', 'family guy'),
-('Брайн Грифен', 'm', 'good', 'family guy'),
+('Брайн Грифен', 'm', 'neutral', 'family guy'),
 ('Мег Грифен', 'f', 'good', 'family guy'),
-('Стьюи Грифен', 'm', 'good', 'family guy'),
+('Стьюи Грифен', 'm', 'evil', 'family guy'),
 ('Крис Грифен', 'm', 'good', 'family guy'),
 
 -- the simpsons
@@ -88,14 +99,14 @@ INSERT INTO `tour`.`pers` (`name`, `sex`, `alignment`, `universe`) VALUES
 -- american dad
 ('Стен Смит', 'm', 'good', 'american dad'),
 ('Франсин Смит', 'f', 'good', 'american dad'),
-('Роджер', 'm', 'good', 'american dad'),
+('Роджер', 'm', 'neutral', 'american dad'),
 ('Хели Смит', 'f', 'good', 'american dad'),
 ('Стив Смит', 'm', 'good', 'american dad'),
 
 -- bojack horseman
-('Конь Боджек', 'm', 'good', 'bojack horseman'),
+('Конь Боджек', 'm', 'neutral', 'bojack horseman'),
 ('Принцеса Керолин', 'f', 'good', 'bojack horseman'),
-('Ана Спаникопита', 'f', 'good', 'bojack horseman'),
+('Ана Спаникопита', 'f', 'neutral', 'bojack horseman'),
 ('Диана Нгуейн', 'f', 'good', 'bojack horseman'),
 
 -- the hunger games
@@ -127,14 +138,14 @@ INSERT INTO `tour`.`pers` (`name`, `sex`, `alignment`, `universe`) VALUES
 ('Клэр', 'f', 'good', 'claymore'),
 
 -- gravity falls
-('Бил Сайфер', 'm', 'good', 'gravity falls'),
-('Гидеон Глифул', 'm', 'good', 'gravity falls'),
+('Бил Сайфер', 'm', 'evil', 'gravity falls'),
+('Гидеон Глифул', 'm', 'evil', 'gravity falls'),
 ('Венди Кордрой', 'f', 'good', 'gravity falls'),
 ('Диппер Пайнс', 'm', 'good', 'gravity falls'),
 ('Мейбел Пайнс', 'f', 'good', 'gravity falls'),
 
 -- silent hill
-('Алесса Гиллеспи', 'f', 'good', 'silent hill'),
+('Алесса Гиллеспи', 'f', 'evil', 'silent hill'),
 
 -- ghost in the shell
 ('Мотоко Кусонаги', 'f', 'good', 'ghost in the shell'),
@@ -142,22 +153,22 @@ INSERT INTO `tour`.`pers` (`name`, `sex`, `alignment`, `universe`) VALUES
 -- the matrix
 ('Нео', 'm', 'good', 'the matrix'),
 ('Тринити', 'f', 'good', 'the matrix'),
-('Агент Смит', 'm', 'good', 'the matrix'),
+('Агент Смит', 'm', 'evil', 'the matrix'),
 
 -- star craft
-('Сара Кериган', 'f', 'good', 'star craft'),
+('Сара Кериган', 'f', 'neutral', 'star craft'),
 ('Джим Рейнор', 'm', 'good', 'star craft'),
 ('Мэтт Хорнер', 'm', 'good', 'star craft'),
-('Арктур Менгск', 'm', 'good', 'star craft'),
+('Арктур Менгск', 'm', 'evil', 'star craft'),
 ('Нова', 'f', 'good', 'star craft'),
-('Абатур', 'm', 'good', 'star craft'),
+('Абатур', 'm', 'neutral', 'star craft'),
 
 -- other
 ('Бетмен', 'm', 'good', 'other'),
 ('Супермен', 'm', 'good', 'other'),
 ('Спайдер Мен', 'm', 'good', 'other'),
-('Локи', 'm', 'good', 'other'),
+('Локи', 'm', 'evil', 'other'),
 ('Лилу', 'f', 'good', 'other'),
-('Джек Воробей', 'm', 'good', 'other');
+('Джек Воробей', 'm', 'neutral', 'other');
 
 
